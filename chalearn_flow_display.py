55dd48707055d3879b651a24ec8c330c206af87c
zc402 <zc35402@163.com>
2022-10-20 18:41:17 +0800

    optical flow display

--- chalearn_flow_display.py
+++ chalearn_flow_display.py
@@ -9,6 +9,7 @@ from config.defaults import get_override_cfg
 import numpy as np
 from multiprocessing import Pool
 import sys
+from numpy.linalg import norm 
 
 cfg = get_override_cfg()
 
@@ -23,6 +24,10 @@ for npy_path in npy_list:
     # Range of optical flow: [-5, 5]
     # print(np.array(flow_list).max(), np.array(flow_list).min())
     for f in flow_data:
-        img = ((f[:, :, 0] + 5) / 10 *255).astype(np.uint8)
-        cv2.imshow('win', img)
+        # [-5, 5] -> [0, 1]
+        f01 = (f + 5) / 10
+        M_norm = norm(f01, ord=2, axis=2)  # HWC
+        rgb = np.concatenate([f01, M_norm[:, :, np.newaxis]], axis=2)
+        # img = ( *255).astype(np.uint8)
+        cv2.imshow('win', cv2.resize(rgb, dsize=None, fx=4, fy=4))
         cv2.waitKey(100)
